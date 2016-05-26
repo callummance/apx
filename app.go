9def1462fc909bda39eddb6053094e9c23ac4a3b
Callum Mance <callummance@gmail.com>
2016-05-27 00:12:41 +0100

    Cleaned up code

--- app.go
+++ app.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"github.com/callummance/apx-srv/auth"
 	"github.com/callummance/apx-srv/db"
 	"github.com/callummance/apx-srv/handlers"
 	"github.com/callummance/apx-srv/middlewares"
-	"github.com/callummance/apx-srv/auth"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -13,24 +13,22 @@ import (
 
 func main() {
 
-  db.Connect()
-  router := gin.Default()
+	db.Connect()
+	router := gin.Default()
 
+	// Middlewares
+	router.Use(middlewares.Connect)
+	router.Use(middlewares.ErrorHandler)
 
-  // Middlewares
-  router.Use(middlewares.Connect)
-  router.Use(middlewares.ErrorHandler)
-
-
-  //router.GET("/", func(c *gin.Context) {
-  //  http.ServeFile(c.Writer, c.Request, "index.html")
-  //})
-  router.StaticFS("/home", http.Dir("../../../../../assignments/apex_name_subject_to_change/webpage"))
-  router.GET("/loggedin", func(c *gin.Context){
-    c.Redirect(303, "/home/dashboard.html")
-  })
-  router.GET("/fbauth", auth.AuthHandler)
-  router.GET("/", handlers.LandingHandler)
-  router.Run(":80")
+	//router.GET("/", func(c *gin.Context) {
+	//  http.ServeFile(c.Writer, c.Request, "index.html")
+	//})
+	router.StaticFS("/home", http.Dir("../../../../../apx/webpage"))
+	router.GET("/loggedin", func(c *gin.Context) {
+		c.Redirect(303, "/home/dashboard.html")
+	})
+	router.GET("/fbauth", auth.AuthHandler)
+	router.GET("/", handlers.LandingHandler)
+	router.Run(":80")
 
 }
--- auth/auth.go
+++ auth/auth.go
@@ -1,13 +1,13 @@
 package auth
 
 import (
-  "errors"
-  "github.com/callummance/apx-srv/models"
-  "gopkg.in/mgo.v2"
-  "gopkg.in/mgo.v2/bson"
-  "github.com/gin-gonic/gin"
-  "fmt"
-  "time"
+	"errors"
+	"fmt"
+	"github.com/callummance/apx-srv/models"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
 )
 
 const defaultAvatar string = ""
@@ -17,7 +17,7 @@ const sessionCookieName string = "apx_session"
 func BuildUser(details FbDetails) models.User {
 	userDetails := getUserAdvancedDetails(details.User_id)
 	var newUser models.User
-        newUser.Id = bson.NewObjectId()
+	newUser.Id = bson.NewObjectId()
 	newUser.FId = details.User_id
 	newUser.Name = userDetails.Name
 	newUser.Email = userDetails.Email
@@ -30,7 +30,7 @@ func BuildUser(details FbDetails) models.User {
 //user ID it was assigned to
 func AuthenticateSession(sid string, mdb *mgo.Database) string {
 	//Lookup session id
-        
+
 	//Check if it is valid and has not expired
 	//Return uid if true
 	//Otherwise return empty string
@@ -38,79 +38,80 @@ func AuthenticateSession(sid string, mdb *mgo.Database) string {
 }
 
 func AuthHandler(c *gin.Context) {
-  authToken := c.Query("auth_token")
-  mdb := c.MustGet("db").(*mgo.Database)
-  uid, err := AuthenticateUser(authToken, mdb)
-  if (err != nil) {
-    //Token is invalid
-    panic("Token error, should probably handle this better...")
-  }
-  sessionKey := NewSession(uid, mdb)
-  c.SetCookie(sessionCookieName, sessionKey.SessionKey, sessionDuration, "/", "apxtest.twintailsare.moe", false, false)
-  sessionUser, err := AuthSession(c, mdb)
-  fmt.Printf("Session cookie found for %q\n", sessionUser)
+	authToken := c.Query("auth_token")
+	mdb := c.MustGet("db").(*mgo.Database)
+	uid, err := AuthenticateUser(authToken, mdb)
+	if err != nil {
+		//Token is invalid
+		panic("Token error, should probably handle this better...")
+	}
+	sessionKey := NewSession(uid, mdb)
+	c.SetCookie(sessionCookieName, sessionKey.SessionKey, sessionDuration, "/", "apx.twintailsare.moe", false, false)
+	fmt.Printf("Creating a new cookie with key %s", sessionKey.SessionKey)
+	sessionUser, err := AuthSession(c, mdb)
+	fmt.Printf("Session cookie found for %q\n", sessionUser)
 }
 
 func AuthSession(c *gin.Context, mdb *mgo.Database) (*bson.ObjectId, error) {
-  sessionKey, err := c.Cookie(sessionCookieName)
-  if (err != nil) {
-    fmt.Println("no cookie found")
-    return nil, err
-    //Cookie not found, apparently...
-  }
-  //Lookup session in database
-  query := mdb.C(models.CollectionSessions).Find(bson.M{"session_key": sessionKey}) 
-  n, err := query.Count()
-  if (err != nil) {
-    fmt.Println("couldnt run query")
-    return nil, err
-  }
-  if (n == 0) {
-    fmt.Println("no matching cookies")
-    return nil, errors.New("No such session")
-  }
+	sessionKey, err := c.Cookie(sessionCookieName)
+	if err != nil {
+		fmt.Println("no cookie found")
+		return nil, err
+		//Cookie not found, apparently...
+	}
+	//Lookup session in database
+	query := mdb.C(models.CollectionSessions).Find(bson.M{"session_key": sessionKey})
+	n, err := query.Count()
+	if err != nil {
+		fmt.Println("couldnt run query")
+		return nil, err
+	}
+	if n == 0 {
+		fmt.Println("no matching cookies")
+		return nil, errors.New("No such session")
+	}
 
-  results := query.Iter()
+	results := query.Iter()
 
-  var session models.Session
-  currentTime := time.Now().UTC().Unix()
-  for results.Next(&session) {
-    if (session.Expires < currentTime){
-      mdb.C(models.CollectionSessions).RemoveId(session.Id)
-    } else {
-      return &session.UID, nil
-    }
-  }
-  return nil, errors.New("No matching session")
+	var session models.Session
+	currentTime := time.Now().UTC().Unix()
+	for results.Next(&session) {
+		if session.Expires < currentTime {
+			mdb.C(models.CollectionSessions).RemoveId(session.Id)
+		} else {
+			return &session.UID, nil
+		}
+	}
+	return nil, errors.New("No matching session")
 }
 
 func AuthenticateUser(token string, mdb *mgo.Database) (bson.ObjectId, error) {
-  userDetails := GetUserDetails(token)
-  if !userDetails.Is_valid {
-    return "", errors.New("Token provided is not valid or has expired.")
-  }
+	userDetails := GetUserDetails(token)
+	if !userDetails.Is_valid {
+		return "", errors.New("Token provided is not valid or has expired.")
+	}
 
-  //Lookup the user in database
-  uid := userDetails.User_id
-  user := models.User{}
-  query := mdb.C(models.CollectionUsers).Find(bson.M{"fid": uid}) 
-  no, err := query.Count()
-  if (err != nil) {
-    return "", err
-  } else if (no != 0) {
-    //If the user exists, return the user id and nil
-    err := query.One(&user)
-    fmt.Printf("Welcome back to the old user! UID = %s\n",
-               uid)
-    if (err != nil) {
-      return "", err
-    }
-  } else {
-    //Otherwise, make a new user with BuildUser, and add it to the database
-    user = BuildUser(userDetails)
-    mdb.C(models.CollectionUsers).Insert(user)
-    fmt.Printf("Welcome to the new user! UID = %s, Name = %s\n",
-               uid, user.Name)
-  }
-  return user.Id, nil
+	//Lookup the user in database
+	uid := userDetails.User_id
+	user := models.User{}
+	query := mdb.C(models.CollectionUsers).Find(bson.M{"fid": uid})
+	no, err := query.Count()
+	if err != nil {
+		return "", err
+	} else if no != 0 {
+		//If the user exists, return the user id and nil
+		err := query.One(&user)
+		fmt.Printf("Welcome back to the old user! UID = %s\n",
+			uid)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		//Otherwise, make a new user with BuildUser, and add it to the database
+		user = BuildUser(userDetails)
+		mdb.C(models.CollectionUsers).Insert(user)
+		fmt.Printf("Welcome to the new user! UID = %s, Name = %s\n",
+			uid, user.Name)
+	}
+	return user.Id, nil
 }
