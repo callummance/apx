a60770fd85134d846b1943b0ccd12203857fd221
Callum Mance <callummance@gmail.com>
2016-06-02 19:20:05 +0100

    added testin features

--- handlers/apiuser.go
+++ handlers/apiuser.go
@@ -20,6 +20,10 @@ func getUser (c *gin.Context) {
     c.String(403, "{\"code\": 1004, \"message\": \"User has set their profile to private\"}")
   } else {
     c.JSON(200, target)
+    c.JSON(200, target)
+    c.JSON(200, target)
+    c.JSON(200, target)
+    c.JSON(200, target)
   }
 }
 
